6efb61f9193a6d2d4a67d8e669e6a6f0ee398594
yuichikano <58720709+yuichikano@users.noreply.github.com>
2021-05-23 11:15:35 +0900

    second commit

--- Frame_analysis.py
+++ Frame_analysis.py
@@ -2,35 +2,57 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageTk
 
+# グローバル変数
+img_paths = ["images/03_1_front.jpg","images/03_2_side.jpg","images/03_3_back.jpg"]
+img_index = 0
+img_path  = img_paths[img_index]
+
+
 root = Tk()
 root.title("XXX")
 
 frame = ttk.Frame(root, padding=16)
-label = ttk.Label(frame, text="name")
-t = StringVar()
-entry = ttk.Entry(frame, textvariable=t)
-button = ttk.Button(
-    frame,
-    text="OK",
-    command=lambda:print(t.get())
-) 
-canvas = Canvas(
-    frame,
-    width=500,
-    height=500
-)
-img_paths = ["images/03_1_front.jpg","rb","images/03_2_side.jpg","rb","images/03_3_back.jpg","rb"]
 
-img =Image.open(open("images/03_1_front.jpg","rb"))
+canvas = Canvas(frame, width=500, height=500)
+
+img =Image.open(open(img_path,"rb"))
 img = ImageTk.PhotoImage(img)
 canvas.create_image(0,0,image=img,anchor=NW)
 
-frame.pack()
-label.pack(side=LEFT)
-entry.pack(side=LEFT)
-button.pack(side=LEFT)
-canvas.pack(side=LEFT)
+def change_image(key):
+    canvas.delete("all")
+    global img_paths, img_path,img,img_index
+    if key == "Right":
+        if img_index < len(img_paths)-1:
+            img_index += 1
+        else:
+            img_index = 0
+    elif key == "Left":
+        if img_index == 0:
+            img_index = len(img_paths) -1
+        else:
+            img_index -=1
 
-# https://www.rumadra.com/2019/06/14/python-tkinter-keyboard/
-# このページからイベント処理を記述して特定の操作によって変化するようにする
+    img_path = img_paths[img_index]
+    print("変更後のpath"+img_path)
+    img = Image.open(open(img_path,"rb"))
+    img = ImageTk.PhotoImage(img)
+    canvas.create_image(0,0,image=img,anchor=NW)
+
+def change_image_bykey(event):
+    key = event.keysym
+    change_image(key)
+
+button_left = ttk.Button(frame, text="<-", command=lambda: change_image("Left"))
+button_right = ttk.Button(frame, text="->", command=lambda: change_image("Right"))
+
+frame.pack()
+canvas.pack(side=LEFT)
+button_left.pack(side=LEFT)
+button_left.bind("<Key>", change_image_bykey)
+button_left.focus_set()
+button_right.pack(side=LEFT)
+button_right.bind("<Key>", change_image_bykey)
+button_right.focus_set()
+
 root.mainloop()
